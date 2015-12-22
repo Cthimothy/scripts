295f915aca1100ef3253129c18bfd0ccd81b17b2
timjbw <timw@miskatonic-systems.com>
2015-12-22 11:46:27 +0000

    Fixed up mocp_cd

--- mocp_cd/mocp_cd.sh
+++ mocp_cd/mocp_cd.sh
@@ -1,6 +1,6 @@
 echo; echo "'Pushd'-ing current directory...'popd' to return."; echo
 pushd . > /dev/null 2>&1
-FOO=$(mocp -i|grep 'File:'|cut -d '/' -f2-6)
-SLASH='/'
-MOCP_ALBUM_PATH=$SLASH$FOO
-cd "$MOCP_ALBUM_PATH"
+foo=$(mocp -i|grep 'File:'|cut -d '/' -f2-6)
+slash='/'
+mocp_album_path=$slash$foo
+cd "$mocp_album_path"
