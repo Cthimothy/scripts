d19c7bb33fc604e4618000d1d6cd0882fad37120
timjbw <timw@miskatonic-systems.com>
2015-12-30 17:39:18 +0000

    Also display other covers

--- mocp_covers/mocp_covers.sh
+++ mocp_covers/mocp_covers.sh
@@ -2,7 +2,7 @@
 
 ## TODO check for existence of mp3 tags
 
-mfile=; fulldir=; foo=; slash=; front=; mocp_album_path=; artist=; album=; glurk=;
+mfile=; fulldir=; foo=; slash=; front=; mocp_album_path=; artist=; album=; glurk=; other_covers=
 slash="/"
 front="front.jpg"
 clear
@@ -12,10 +12,13 @@ if [ -n "$mfile" ]; then
     fulldir=$(dirname "$mfile")
 fi
 mocp_album_art=$fulldir$slash$front
-fi
+other_covers=$(find "$fulldir" -type f \( ! -iname front.jpg -iname \*.jpg -o -iname \*.gif -o -iname \*.png \))
 
 if [ -f "$mocp_album_art" ]; then
     feh "$mocp_album_art"
+    if [ -z "$other_covers" ]; then
+        feh "$other_covers"
+    fi
 else
     echo "[+] Album art not found..."
     #foo=$(mocp -i|grep 'File:'|cut -d '/' -f2-6)
@@ -36,5 +39,8 @@ else
     echo "[+] Done."
     mocp_album_art="$mocp_album_path$front"
     clear
-    feh "$mocp_album_art"
+    feh "$mocp_album_art" "$other_covers"
+    if [ -z "$other_covers" ]; then
+        feh "$other_covers"
+    fi
 fi
