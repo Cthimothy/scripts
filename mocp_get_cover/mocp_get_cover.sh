4e1a9dc095ff5f39c7417e76de69eda8ecc6398d
timjbw <timw@miskatonic-systems.com>
2015-12-30 15:01:38 +0000

    Fixed mocp_get_covers for subdirectories

--- mocp_get_cover/mocp_get_cover.sh
+++ mocp_get_cover/mocp_get_cover.sh
@@ -1,14 +1,22 @@
-
 #!/usr/bin/env bash
-foo=; slash=; front=; mocp_album_path=; artist=; album=; glurk=; cover_file_name=
 clear
-foo=$(mocp -i|grep 'File:'|cut -d '/' -f2-6)
+foo=; slash=; front=; mocp_album_path=; artist=; album=; glurk=; cover_file_name=
+
 slash="/"
 front="front.jpg"
-mocp_album_path="$slash$foo"
+#foo=$(mocp -i|grep 'File:'|cut -d '/' -f2-6)
+test_dir=$(mocp -i|grep 'File:'|cut -d '/' -f6)
+if [ "$test_dir" == "___0SDM-Demos" ] ; then
+    foo=$(mocp -i|grep 'File:'|cut -d '/' -f2-7)
+else
+    foo=$(mocp -i|grep 'File:'|cut -d '/' -f2-6)
+fi
+mocp_album_path="$slash$foo$slash"
+
 echo "[+] Getting album information..."
 artist=$(mocp -i | grep ^Artist | cut -d " " -f2-)
 album=$(mocp -i | grep ^Album | cut -d " " -f2-)
+
 echo "[+] Downloading cover art for $album..."
 glurk=$(glyrc cover -a "$artist" -b "$album"  2>&1 | grep WRITE  | cut -d "/" -f2) # yuk
 cover_file_name="${glurk%?}"
